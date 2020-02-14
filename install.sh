d7e67e8b74d423016b9e66afd9ac8cd9c4b4a7b4
Radimir Mikhailov <radimir.com@gmail.com>
2020-02-14 13:09:36 +0700

    Добавлен скрипт первоначальной настройки

---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+# Based on https://github.com/elnappo/dotfiles/blob/master/init/setup.sh
+
+set -e
+
+title() {
+    local color='\033[1;37m'
+    local nc='\033[0m'
+    printf "\n${color}$1${nc}\n"
+}
+
+title "[i] Ask for sudo password"
+sudo -v
+
+case "$(uname -s)" in
+
+    Linux)
+        if [ -f /etc/os-release ]
+        then
+            source /etc/os-release
+
+            case "$ID_LIKE" in
+
+                debian | ubuntu)
+                    title "[i] Install Ansible"
+                    sudo apt-get install -y ansible
+                    ;;
+
+                *)
+                    title "[!] Unsupported Linux Distribution: $ID (like $ID_LIKE)"
+                    exit 1
+                    ;;
+            esac
+        else
+            title "[!] Unsupported Linux Distribution"
+            exit 1
+        fi
+        ;;
+
+    *)
+        title "[!] Unsupported OS"
+        exit 1
+        ;;
+esac
+
+title "[i] Run Playbook"
+#ansible-playbook $HOME/dotfiles/ansible/dotfiles.yml --ask-become-pass
+
+title "[i] Done."
