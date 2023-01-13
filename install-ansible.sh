4d3771e328309343e73a0b9aaa993f2b0432696f
Radimir Mikhailov <radimir.com@gmail.com>
2023-01-13 16:45:33 +0700

    Добавлен недостающий параметр при установке Ansible под Ubuntu

--- install-ansible.sh
+++ install-ansible.sh
@@ -27,7 +27,7 @@ else
                         ;;
                     ubuntu)
                         sudo apt-get update
-                        sudo apt-get install software-properties-common
+                        sudo apt-get install -y software-properties-common
                         sudo add-apt-repository --yes --update ppa:ansible/ansible
                         sudo apt-get install -y ansible
                         ;;
